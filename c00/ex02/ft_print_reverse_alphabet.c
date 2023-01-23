b486e00213a6f94f22dec013ea7158dd76c8bc5e
Gabriel Beaulieu Robitaille <gbeaulie@c1r8p4.42quebec.com>
2023-01-23 11:36:06 -0500

    Study Harder

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbeaulie <gbeaulie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 14:19:47 by gbeaulie          #+#    #+#             */
+/*   Updated: 2023/01/20 12:40:27 by gbeaulie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
---
+++ c01/ex02/ft_swap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbeaulie <gbeaulie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 09:03:21 by gbeaulie          #+#    #+#             */
+/*   Updated: 2023/01/23 06:55:22 by gbeaulie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+// int	main(void)
+// {
+// 	int	value1;
+// 	int	value2;
+
+// 	value1 = 99;
+// 	value2 = 66;
+// 	printf("Before Swapping: value1 is: %d, value2 is: %d\n", value1, value2);
+// 	ft_swap(&value1, &value2);
+// 	printf("After Swapping: value1 is: %d, value2 is: %d\n", value1, value2);
+// 	return (0);
+// }
